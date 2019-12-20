f2bc96a65ff2298f644182ddbf68d1d6af5a4b1c
perseveances <tao.best@foxmail.com>
2019-12-20 22:35:31 +0800

    更改错误

--- 前端/像素鸟小游戏/index.js
+++ 前端/像素鸟小游戏/index.js
@@ -122,6 +122,132 @@ bird = {
 			this.dom.style.backgroundPosition = '-8px -10px';
 		}else if(this.wingIndex === 1){
 			this.dom.style.backgroundPosition = '-60px -10px';
+		}else{
+			this.dom.style.backgroundPositon = '-113px -10px';
+		}
+		//设置小鸟的top值
+		this.dom.style.top = this.top + 'px';
+	},
+	//设置小鸟的top值
+	setTop(newTop){
+		if(newTop < 0){
+			newTop = 0;
+		}
+		if(newTop > 462){
+			newTop = 462;
+		}
+		this.top = newTop;
+	},
+	jump(){
+		this.speed = -0.5;
+	}
+}
+
+//让小鸟不停的扇动翅膀的计时器
+bird.wingTimer = getTimer(100,bird,function(){
+	//这里面主要要做的事儿：修改wingIndex，然后调用show
+	this.wingIndex = (this.wingIndex +1 )%3;
+	this.show();
+});
+bird.dropTimer = getTimer(16,bird,function(){
+	//主要要做的事儿：改变高度，然后调用setTop方法以及show方法
+	//小鸟做的事匀加速运动
+	//s = vt + 1/2 * a * t * t
+	//如何获取匀加速的末速度：v = v0 + at
+	let s this.speed * 16 + 0.5 * this.a * 16 * 16;
+	this.speed = this.speed + this.a * 16;
+	this.setTop(this.top + s);
+	this.show();
+});
+
+//上下的管道
+pipes = {
+	width:52,
+	getRandom:function(min,max){
+		return Math,floor(Math.random() * (max - min) + min);
+	},
+	all:[],		//用于存放所有的柱子
+	//创建柱子的方法
+	createPipe(){
+		let minHeight = 60,		//柱子最小的高度
+		gap = 150;				//中间的间隙
+		maxHeight = 488 - gap - minHeight;
+		
+		let h1 = this.getRandom(minHeight,maxHeight),
+		h1 = 488 - gap - h1;
+		//接下来根据这两个高度来创建柱子
+		//上面的柱子
+		let div1 = document.createElemet("div");
+		div1.classNmae = "pipeip";
+		div1.style.height = h1 + "px";
+		div1.style.left = "800px";
+		game.som.appendChild(div1);
+		this.all.push({
+			dom:div1;
+			height:h1;
+			width:this.width;
+			top:0;
+			left:800
+		});
+		//下面的柱子
+		let div2 = document.createElement("div");
+		div2.classNmae = "pipedown";
+		div2.style.height = h2 + "px";
+		div2.style.left = "800px";
+		game.dom.appendChild(div2);
+		this.all.push({
+			dom:div2,
+			height:h2,
+			width:this.width,
+			top:h1 + gap,
+			left:800
+		});
+	},
+}
+
+//创建柱子的方法
+pipes.produceTimer = getTimer(2500,pipes,function(){
+	this.createPipe();
+})
+
+//移动柱子
+Pipes.moveTimer = getTimer(30,pipes,function(){
+	//因为要移动所有的柱子  &&  对游戏进行积分
+	for(let i=0;i<this.all.length;i++){
+		let p = this.all[i];	//得到当前的柱子
+		p.left -= 2;
+		if(p.left < -p.width){
+			p.dom.remove();
+			this.all.splice(i,1);
+			i--;
+		}else{
+			p.dom.style.left = p.left + 'px';
+		}
+		
+		//判断柱子是否过了小鸟，若过了则说明小鸟过了一根柱子
+		if(p.left< (bird.left-pipes.width)){
+			comsole("+1");
+		}
+	}
+	
+	game.gameOver();		//每次移动柱子后，都需要判断游戏是否结束
+})；
+
+document.documentElement.onkeydown = function(e){
+	if(e.key === ' '){
+		bird.jump();
+	}
+	if(e.key === 'Enter'){
+		//按下回车键后，有三种状态（游戏运行中，游戏暂停中，游戏结束后）
+		if(game.isGameOver){
+			location.reload();
+		}
+		if(game.paused){
+			game.start();
+			game.paused = !game.paused
+		}else{
+			game.stop();
+			game.paused = !game.paused
 		}
 	}
 }
